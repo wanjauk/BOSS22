fcfb5cfa3a4f79e786dacbec5dabbdcf35c12d9d
LandiMi2 <LandiMi2@users.noreply.github.com>
2022-03-16 07:03:31 +0000

    jekyll build from Action b7468d592ded1b810dcfc470251b42c2f87ad333

---
+++ sw/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Karibu kwa Jekyll!",
+        "excerpt":"Utaona maandiko hii kwa _posts/sw. Jaribu kubadili faili na kutengeneza tovuti tena kuona mabadilisho yako. Unaweza kutengeneza tovuti kwa njia mingi, lakini watu wengi wanapenda kutumia jekyll serve, itaanzisha seva na itatengeza tovuti mpya ukibadilisha faili. Jekyll inahitaji faili ya blog zina mtindo wa jina ikiwa: MWAKA-MWEZI-SIKU-kichwa.AINA_YA_FAILI MWAKA ni nambari...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2022/02/23/welcome-to-jekyll.html",
+        "teaser": null
+      }]
